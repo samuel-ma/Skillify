3393bb4679c0750e64099c64407ce87097598a4e
Samuel <82509653+samuel-ma@users.noreply.github.com>
2023-01-15 16:59:14 +0200

    Delete src directory

--- src/components/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Avatar({src, alt, size='sm'}) {
-  return (
-    <div className="user-avatar">
-        <img className={`rounded-circle ${size}`} src={src} alt={alt} />
-    </div>
-  )
-}
--- src/components/RoundedIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function RoundedIcon({icon, text=''}) {
-  return (
-    <div className='rounded-icon rounded-circle d-flex align-items-center justify-content-center'>
-        {icon}
-    </div>
-  )
-}
